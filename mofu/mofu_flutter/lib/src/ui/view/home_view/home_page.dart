03e9726210518997cccf2e147847afa8a47a23cb
Ha JongMyeong <36729917+kumangho@users.noreply.github.com>
2022-03-27 20:43:55 +0900

    commit

--- mofu/mofu_flutter/lib/src/ui/view/home_view/home_page.dart
+++ mofu/mofu_flutter/lib/src/ui/view/home_view/home_page.dart
@@ -57,161 +57,158 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            child: Column(
-              children: [
-                Container(
-                  
+          Column(
+            children: [
+              Container(
                 width: displayWidth * 0.8,
-                  child: Text(
-                    'Order by : life',
-                    style: TextStyle(
-                        color: Colors.black54, fontWeight: FontWeight.bold,
-                        fontSize: displayHeight * 0.01),
-                  ),
-                  alignment: Alignment.bottomRight,
+                child: Text(
+                  'Order by : life',
+                  style: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold,
+                      fontSize: displayHeight * 0.01),
                 ),
-                Container(
-                    child: Stack(
-                  children: [
-                    Container(
-                      child: CustomRadioButton(
-                        defaultSelected: "All",
-                        autoWidth: false,
-                        width: displayWidth * 0.17,
-                        height: displayHeight * 0.03,
-                        customShape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                            side: BorderSide(color: mainColor)),
-                        enableShape: true,
-                        elevation: 0,
-                        absoluteZeroSpacing: false,
-                        spacing: displayWidth * 0.01,
-                        padding: displayHeight * 0.004,
-                        selectedBorderColor: mainColor,
-                        unSelectedColor: Colors.white,
-                        unSelectedBorderColor: mainColor,
-                        buttonLables: [
-                          'All',
-                          'Good',
-                          'Fridged',
-                          'Danger',
-                        ],
-                        buttonValues: [
-                          'All',
-                          "Good",
-                          "Fridged",
-                          "Danger",
-                        ],
-                        buttonTextStyle: ButtonTextStyle(
-                            selectedColor: Colors.white,
-                            unSelectedColor: mainColor,
-                            textStyle: TextStyle(
-                                fontSize: displayHeight * 0.013,
-                                fontWeight: FontWeight.bold)),
-                        radioButtonValue: (value) {
-                          print(value);
-                        },
-                        selectedColor: mainColor,
-                      ),
+                alignment: Alignment.bottomRight,
+              ),
+              Container(
+                  child: Stack(
+                children: [
+                  Container(
+                    child: CustomRadioButton(
+                      defaultSelected: "All",
+                      autoWidth: false,
+                      width: displayWidth * 0.17,
+                      height: displayHeight * 0.03,
+                      customShape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          side: BorderSide(color: mainColor)),
+                      enableShape: true,
+                      elevation: 0,
+                      absoluteZeroSpacing: false,
+                      spacing: displayWidth * 0.01,
+                      padding: displayHeight * 0.004,
+                      selectedBorderColor: mainColor,
+                      unSelectedColor: Colors.white,
+                      unSelectedBorderColor: mainColor,
+                      buttonLables: [
+                        'All',
+                        'Good',
+                        'Fridged',
+                        'Danger',
+                      ],
+                      buttonValues: [
+                        'All',
+                        "Good",
+                        "Fridged",
+                        "Danger",
+                      ],
+                      buttonTextStyle: ButtonTextStyle(
+                          selectedColor: Colors.white,
+                          unSelectedColor: mainColor,
+                          textStyle: TextStyle(
+                              fontSize: displayHeight * 0.013,
+                              fontWeight: FontWeight.bold)),
+                      radioButtonValue: (value) {
+                        print(value);
+                      },
+                      selectedColor: mainColor,
                     ),
-                    /** 
-                    Row(//left align needed
-                      children: [
-                        Container(
-                            padding: EdgeInsets.fromLTRB(
-                                displayWidth*0.13, 8, 0, 0),
-                            child: const Image(
-                                image: AssetImage(
-                                    'assets/images/icons/expiration_icon/good.png'))),
-                        Container(
-                            padding: EdgeInsets.fromLTRB(
-                                0, 8, 0, 0),
-                            child: Image(
-                                image: const AssetImage(
-                                    'assets/images/icons/expiration_icon/fridged.png'))),
-                        Container(
-                          padding:
-                              EdgeInsets.fromLTRB(0, 8, 0, 0),
+                  ),
+                  /** 
+                  Row(//left align needed
+                    children: [
+                      Container(
+                          padding: EdgeInsets.fromLTRB(
+                              displayWidth*0.13, 8, 0, 0),
+                          child: const Image(
+                              image: AssetImage(
+                                  'assets/images/icons/expiration_icon/good.png'))),
+                      Container(
+                          padding: EdgeInsets.fromLTRB(
+                              0, 8, 0, 0),
                           child: Image(
                               image: const AssetImage(
-                                  'assets/images/icons/expiration_icon/danger.png')),
-                        ),
-                        Container(
-                            padding: EdgeInsets.fromLTRB(
-                                0, 8, 0, 0),
-                            child: const Image(
-                                image: AssetImage(
-                                    'assets/images/icons/expiration_icon/good.png'))),
-                      ],
-                    ),*/
-                  ],
-                )),
-                Container(
-                  height: 2,
-                  width: displayWidth * 0.8,
-                  color: mainColor,
+                                  'assets/images/icons/expiration_icon/fridged.png'))),
+                      Container(
+                        padding:
+                            EdgeInsets.fromLTRB(0, 8, 0, 0),
+                        child: Image(
+                            image: const AssetImage(
+                                'assets/images/icons/expiration_icon/danger.png')),
+                      ),
+                      Container(
+                          padding: EdgeInsets.fromLTRB(
+                              0, 8, 0, 0),
+                          child: const Image(
+                              image: AssetImage(
+                                  'assets/images/icons/expiration_icon/good.png'))),
+                    ],
+                  ),*/
+                ],
+              )),
+              Container(
+                height: 2,
+                width: displayWidth * 0.8,
+                color: mainColor,
+              ),
+              Container(
+                width: displayWidth * 0.8,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
                 ),
-                Container(
-                  width: displayWidth * 0.8,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  margin: EdgeInsets.only(
-                      left: displayWidth * 0.02, right: displayWidth * 0.02),
-                  padding: EdgeInsets.only(
-                      top: displayHeight * 0.01),
-                  height: displayHeight * 0.2,
-                  child: GridView.builder(
-                      itemCount: 7,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 5,
-                          crossAxisSpacing: displayWidth * 0.02,
-                          mainAxisSpacing: displayHeight * 0.02),
-                      itemBuilder: (BuildContext context, int index) {
-                        return Stack(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)),
-                                color: Colors.white,
-                                border: Border.all(color: mainColor, width: 2),
-                              ),
-                              child: const Image(
-                                image: AssetImage(
-                                    'assets/images/icons/ingredient_icon/bacon.png'),
-                              ),
+                margin: EdgeInsets.only(
+                    left: displayWidth * 0.02, right: displayWidth * 0.02),
+                padding: EdgeInsets.only(top: displayHeight * 0.01),
+                height: displayHeight * 0.2,
+                child: GridView.builder(
+                    itemCount: 7,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 5,
+                        crossAxisSpacing: displayWidth * 0.02,
+                        mainAxisSpacing: displayHeight * 0.02),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Stack(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              color: Colors.white,
+                              border: Border.all(color: mainColor, width: 2),
                             ),
-                            Container(
-                                alignment: Alignment.bottomRight,
-                                child: const Image(
-                                    image: AssetImage(
-                                        'assets/images/icons/expiration_icon/fridged.png')))
-                          ],
-                        );
-                      }),
-                )
-              ],
-            ),
+                            child: const Image(
+                              image: AssetImage(
+                                  'assets/images/icons/ingredient_icon/bacon.png'),
+                            ),
+                          ),
+                          Container(
+                              alignment: Alignment.bottomRight,
+                              child: const Image(
+                                  image: AssetImage(
+                                      'assets/images/icons/expiration_icon/fridged.png')))
+                        ],
+                      );
+                    }),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: displayHeight * 0.03),
+                height: 1,
+                width: displayWidth * 0.8,
+                color: mainColor,
+              ),
+              Container(
+                width: displayWidth * 0.8,
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(top: displayHeight * 0.05),
+                child: Text(
+                  '레시피 즐겨찾기',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: displayHeight * 0.02),
+                ),
+              ),
+            ],
           ),
-          Container(
-            margin: EdgeInsets.only(top: displayHeight * 0.03),
-            height: 1,
-            width: displayWidth * 0.8,
-            color: mainColor,
-          ),
-          Container(
-            width: displayWidth * 0.8,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(top: displayHeight * 0.05),
-            child: Text(
-              '레시피 즐겨찾기',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold, fontSize: displayHeight * 0.02),
-            ),
-          ),
-          Container()
         ],
       ),
     ));
