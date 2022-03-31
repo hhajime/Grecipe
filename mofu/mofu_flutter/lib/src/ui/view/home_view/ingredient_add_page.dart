2a2d4050cabfec8f3ee04b3200f10f8a3d273d0d
Ha JongMyeong <36729917+kumangho@users.noreply.github.com>
2022-03-31 22:05:16 +0900

    commit

--- mofu/mofu_flutter/lib/src/ui/view/home_view/ingredient_add_page.dart
+++ mofu/mofu_flutter/lib/src/ui/view/home_view/ingredient_add_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mofu_flutter/src/data/list.dart';
+import 'package:custom_radio_grouped_button/custom_radio_grouped_button.dart';
 
 class IngredientAddPage extends StatelessWidget {
   @override
@@ -7,75 +8,136 @@ class IngredientAddPage extends StatelessWidget {
     return Container(
       color: mainColor,
       child: SafeArea(
-        bottom: false,
+          bottom: false,
           child: Container(
-        color: Colors.white,
-        child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            backgroundColor: mainColor,
-            title: Text(
-              '식재료 등록',
-              style: TextStyle(fontWeight: FontWeight.w800),
-            ),
-          ),
-          body: Column(
-            children: [
-              Container(
-                  width: displayWidth * 0.8,
-                  padding: EdgeInsets.only(top: displayHeight * 0.02),
-                  child: Container(
-                      height: displayHeight * 0.2,
-                      child: Row(
-                        children: [
-                          Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+            color: Colors.white,
+            child: Scaffold(
+              appBar: AppBar(
+                leading: BackButton(
+                  color: mainColor,
+                ),
+                centerTitle: true,
+                backgroundColor: Colors.white,
+                elevation: 0,
+                title: Text(
+                  '식재료 등록',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w800, color: Colors.black),
+                ),
+              ),
+              body: Column(
+                children: [
+                  Container(
+                    height: displayHeight * 0.15,
+                      width: displayWidth * 0.8,
+                      padding: EdgeInsets.only(top: displayHeight * 0.02),
+                      child: Container(
+                          height: displayHeight * 0.2,
+                          child: Row(
                             children: [
-                              Container(
-                                child: Text('이름 입력'),
-                              ),
-                              Container(
-                                child: Container(
-                                    width: displayWidth * 0.3,
-                                    child: TextFormField(
+                              Expanded(
+                                  child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    child: Text('이름 입력'),
+                                  ),
+                                  Container(
+                                    child: Container(
+                                        padding:
+                                            EdgeInsets.only(left: 5, right: 5),
+                                        decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(10)),
+                                          color: textfieldColor,
+                                        ),
+                                        width: displayWidth * 0.34,
+                                        height: displayHeight * 0.06,
+                                        child: TextFormField(
+                                          decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            hintText: '이름을 입력하세요.',
+                                          ),
+                                        )),
+                                  )
+                                ],
+                              )),
+                              Expanded(
+                                  child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text('아이콘으로 등록하기'),
+                                  ),
+                                  Container(
+                                    height: displayHeight * 0.06,
+                                    width: displayHeight * 0.06,
+                                    decoration: BoxDecoration(
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(10)),
+                                      color: subColor,
+                                      border: Border.all(
+                                          color: mainColor, width: 2),
+                                    ),
+                                    child: const Image(
+                                      image: AssetImage(
+                                          'assets/images/icons/ingredient_icon/bacon.png'),
+                                    ),
+                                  )
+                                ],
+                              ))
+                            ],
+                          ))),
+                  Container(
+                    width: displayWidth * 0.8,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(child: Text('유통기한 설정')),
+                        Container(
 
-                                    )),
-                              )
-                            ],
-                          )),
-                          Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                alignment: Alignment.centerLeft,
-                                child: Text('아이콘으로 등록하기'),
+                        child: Row(
+                          children: [],
+                        )
+
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(),
+                  ),
+                  Container(
+                    color: Colors.black,
+                    child: Column(
+                      children: [
+                        Container(
+                          
+                        ),
+                        Container(
+                          child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(10)),
+                                color: textfieldColor,
                               ),
-                              Container(
-                                height: displayHeight * 0.07,
-                                width: displayHeight * 0.07,
-                                decoration: BoxDecoration(
-                                  borderRadius:
-                                      const BorderRadius.all(Radius.circular(15)),
-                                  color: subColor,
-                                  border: Border.all(color: mainColor, width: 2),
+                              width: displayWidth * 0.34,
+                              height: displayHeight * 0.06,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: '내용을 입력하세요.',
                                 ),
-                                child: const Image(
-                                  image: AssetImage(
-                                      'assets/images/icons/ingredient_icon/bacon.png'),
-                                ),
-                              )
-                            ],
-                          ))
-                        ],
-                      ))),
-              Container(),
-              Container()
-            ],
-          ),
-        ),
-      )),
+                              )),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
