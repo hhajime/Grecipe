f421abd91a2b4b206f88900e989b77b8624d3611
Ha JongMyeong <36729917+kumangho@users.noreply.github.com>
2022-04-10 23:53:35 +0900

    commit

--- mofu/mofu_flutter/lib/src/ui/view/home_view/recipe_page.dart
+++ mofu/mofu_flutter/lib/src/ui/view/home_view/recipe_page.dart
@@ -1,36 +1,55 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:mofu_flutter/src/data/model/recipe_model.dart';
 import 'package:mofu_flutter/src/controller/recipe_controller.dart';
 import 'package:get/get.dart';
+import 'package:mofu_flutter/src/data/list.dart';
+import 'package:mofu_flutter/src/ui/widget/landing_page.dart';
 
 class RecipePage extends StatelessWidget {
   final recipeController = Get.put(RecipeController(), permanent: false);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fetch Data Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Fetch Data Example'),
-        ),
-        body: Center(
-          child: FutureBuilder<Recipes>(
-            future: recipeController.recipe,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data!.COOKRCP02.row.first.RCPPARTSDTLS);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
+    return Container(
+      color: mainColor,
+      child: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.white,
+          child: Scaffold(
+            appBar: AppBar(
+                leading: BackButton(
+                  color: mainColor,
+                  onPressed: () {
+                    Get.to(() => LandingPage(),
+                        transition: Transition.cupertino);
+                  },
+                ),
+                centerTitle: true,
+                backgroundColor: Colors.white,
+                elevation: 0,
+                title: Text(
+                  '레시피 이름',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w800, color: Colors.black),
+                ),
+              ),
+            body: Center(
+              child: FutureBuilder<Recipes>(
+                future: recipeController.recipe,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(
+                        snapshot.data!.COOKRCP02.row.first.RCPPARTSDTLS);
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
 
-              // 기본적으로 로딩 Spinner를 보여줍니다.
-              return CircularProgressIndicator();
-            },
+                  // 기본적으로 로딩 Spinner를 보여줍니다.
+                  return CircularProgressIndicator();
+                },
+              ),
+            ),
           ),
         ),
       ),
