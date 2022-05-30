b56269f6e2f58db92d2d0c847a502d732e778fcc
kumangho <impapa68@gmail.com>
2022-05-31 00:11:10 +0900

    commit

--- Grecipe/lib/src/controller/firebase_controller.dart
+++ Grecipe/lib/src/controller/firebase_controller.dart
@@ -2,17 +2,9 @@ import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebaseCloudFireStoreController extends GetxController {
-  final userCollectionReference = FirebaseFirestore.instance
-      .collection("COOKRCP")
-      .doc("j6QXko9oShfPpFpqIWdB");
-
-  void createData() {
-    userCollectionReference.set({});
-  }
-
-  void readData() {
-    userCollectionReference.get().then((value) {
-      print(value.data());
-    });
+  Future readData() async {
+    var _fireStore = FirebaseFirestore.instance;
+    QuerySnapshot snapshot = await _fireStore.collection('COOKRCP').get();
+    return snapshot.docs;
   }
 }
--- Grecipe/lib/src/ui/view/community_view/community_page.dart
+++ Grecipe/lib/src/ui/view/community_view/community_page.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:grecipe/src/data/list.dart';
 import 'package:get/get.dart';
@@ -87,26 +89,21 @@ class CommunityPage extends StatelessWidget {
                                         fontWeight: FontWeight.bold),
                                   )),
                               Container(
-                                height: displayHeight * 0.25,
-                                decoration: BoxDecoration(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(10)),
-                                    color: subColor,
-                                    boxShadow: const [
-                                      BoxShadow(
-                                          offset: Offset(3, 3),
-                                          color: Colors.black12,
-                                          blurRadius: 5,
-                                          spreadRadius: 1)
-                                    ],
-                                    border:
-                                        Border.all(color: mainColor, width: 2)),
-                                /*child: CarouselSlider.builder(
-                                itemCount: itemCount,
-                                itemBuilder: TrendyRecipeBuilder(
-                                    BuildContext(context), itemCount),
-                                options: CarouselOptions())*/
-                              ),
+                                  height: displayHeight * 0.25,
+                                  decoration: BoxDecoration(
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(10)),
+                                      color: subColor,
+                                      boxShadow: const [
+                                        BoxShadow(
+                                            offset: Offset(3, 3),
+                                            color: Colors.black12,
+                                            blurRadius: 5,
+                                            spreadRadius: 1)
+                                      ],
+                                      border: Border.all(
+                                          color: mainColor, width: 2)),
+                                  child: fireStoreOut()),
                             ],
                           ))),
                   Center(
@@ -124,25 +121,90 @@ class CommunityPage extends StatelessWidget {
 
                       /// 한무 스크롤 가능하게
                       child: Container(
-                    height: displayHeight * 1.5,
-                    width: displayWidth * 0.8,
-                    decoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                        color: subColor,
-                        boxShadow: const [
-                          BoxShadow(
-                              offset: Offset(3, 3),
-                              color: Colors.black12,
-                              blurRadius: 5,
-                              spreadRadius: 1)
-                        ],
-                        border: Border.all(color: mainColor, width: 2)),
-                  )),
+                          height: displayHeight * 0.3,
+                          width: displayWidth * 0.8,
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(10)),
+                              color: subColor,
+                              boxShadow: const [
+                                BoxShadow(
+                                    offset: Offset(3, 3),
+                                    color: Colors.black12,
+                                    blurRadius: 5,
+                                    spreadRadius: 1)
+                              ],
+                              border: Border.all(color: mainColor, width: 2)),
+                          child: ListView(
+                            physics: BouncingScrollPhysics(),
+                            shrinkWrap: true,
+                            itemExtent: 100,
+                            children: [
+                              Container(
+                                  color: Colors.green,
+                                  child: Row(
+                                    children: [
+                                      Container(),
+                                      Container(
+                                        child: Column(),
+                                      )
+                                    ],
+                                  )),
+                              Container(
+                                color: Colors.red,
+                              )
+                            ],
+                          ))),
                 ]),
               ),
             ],
           ),
         ));
+  }
+
+  FutureBuilder fireStoreOut() {
+    //추후 carousel slider, on tap적용
+    return FutureBuilder(
+        future: firebaseController.readData(),
+        builder: (_, snapShot) {
+          return snapShot.data == null
+              ? Container(
+                  child: CircularProgressIndicator(),
+                )
+              : Stack(children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                          width: displayWidth * 0.8,
+                          child: Image.network(
+                            snapShot.data[0].data()['COOKRCP']['고구마죽']
+                                ['RCPMAINIMAGE'],
+                            fit: BoxFit.cover,
+                          ))),
+                  Positioned.fill(
+                      child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            height: displayHeight * 0.06,
+                            width: displayWidth * 0.6,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10)),
+                              color: Colors.white.withOpacity(0.7),
+                            ),
+                            child: Column(children: [
+                              Text(
+                                '고구마죽',
+                                style: TextStyle(
+                                    fontSize: displayHeight * 0.02,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(snapShot.data[0].data()['COOKRCP']['고구마죽']
+                                  ['RCP_INTRO'])
+                            ]),
+                          ))),
+                ]);
+        });
   }
 }
